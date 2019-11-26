ab2132eefd0d4f81ee4bd2f4564a5a47b9af0c83
Panji Patria <panjinohnah@gmail.com>
2019-11-26 07:08:18 +0700

    create user to firebase and login with user form firebase

--- app/src/main/java/com/pandev/androidfundamental/chatbot/LoginActivity.java
+++ app/src/main/java/com/pandev/androidfundamental/chatbot/LoginActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.mbms.MbmsErrors;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -16,7 +17,7 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseUser currentUser;
     private Button LoginButton, PhoneLoginButton;
     private EditText UserEmail, UserPassword;
-    private TextView NedNewAccountLink, ForgetPasswordLink;
+    private TextView NeedNewAccountLink, ForgetPasswordLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +25,13 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         InitializeField();
+
+        NeedNewAccountLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SendUserToRegisterActivity();
+            }
+        });
 
     }
 
@@ -33,7 +41,7 @@ public class LoginActivity extends AppCompatActivity {
         PhoneLoginButton = (Button)findViewById(R.id.phone_login_button);
         UserEmail = (EditText)findViewById(R.id.login_email);
         UserPassword = (EditText)findViewById(R.id.login_pass);
-        NedNewAccountLink = (TextView)findViewById(R.id.need_new_account_link);
+        NeedNewAccountLink = (TextView)findViewById(R.id.need_new_account_link);
         ForgetPasswordLink = (TextView)findViewById(R.id.forget_pass_link);
     }
 
@@ -42,12 +50,17 @@ public class LoginActivity extends AppCompatActivity {
         super.onStart();
 
         if (currentUser != null){
-            SendUserMainActivity();
+            SendUserToMainActivity();
         }
     }
 
-    private void SendUserMainActivity() {
-        Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
-        startActivity(mainIntent);
+    private void SendUserToMainActivity() {
+        Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(loginIntent);
+    }
+
+    private void SendUserToRegisterActivity() {
+        Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(registerIntent);
     }
 }
--- app/src/main/java/com/pandev/androidfundamental/chatbot/RegisterActivity.java
+++ app/src/main/java/com/pandev/androidfundamental/chatbot/RegisterActivity.java
@@ -1,14 +1,105 @@
 package com.pandev.androidfundamental.chatbot;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private Button CreateAccountButon;
+    private EditText UserEmail, UserPassword;
+    private TextView AllreadyHaveAccountLink;
+
+    private FirebaseAuth mAuth;
+
+    private ProgressDialog loadingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        InitialField();
+
+        AllreadyHaveAccountLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SendToLoginActivity();
+            }
+        });
+
+
+        CreateAccountButon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CreateNewAccount();
+            }
+        });
+
+    }
+
+
+    private void CreateNewAccount() {
+        String email = UserEmail.getText().toString();
+        String password = UserPassword.getText().toString();
+
+        if (TextUtils.isEmpty(email)){
+            Toast.makeText(this, "Please enter email...", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Please enter password...", Toast.LENGTH_SHORT).show();
+        } else {
+
+            loadingBar.setTitle("Creating New Account");
+            loadingBar.setMessage("Please wait, while we are creating new account for you...");
+            loadingBar.setCanceledOnTouchOutside(true);
+            loadingBar.show();
+
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                SendToLoginActivity();
+                                Toast.makeText(RegisterActivity.this, "Account Created Succesfully...", Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                            } else {
+                                String message = task.getException().toString();
+                                Toast.makeText(RegisterActivity.this, "Error : "+message, Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                            }
+                        }
+                    });
+        }
+    }
+
+    private void InitialField()
+    {
+        CreateAccountButon = (Button) findViewById(R.id.register_button);
+        UserEmail = (EditText) findViewById(R.id.register_email);
+        UserPassword = (EditText) findViewById(R.id.register_pass);
+        AllreadyHaveAccountLink = (TextView) findViewById(R.id.already_have_account_link);
+
+        loadingBar = new ProgressDialog(this);
+    }
+
+    private void SendToLoginActivity(){
+        Intent loginIntent = new Intent(RegisterActivity.this, LoginActivity.class);
+        startActivity(loginIntent);
     }
 }
